a6f6a59934b25d1703f6c44a396243abe81b854d
ekaapsd <epdewi26@gmail.com>
2019-08-21 08:59:07 +0700

    update login

--- src/App.js
+++ src/App.js
@@ -4,36 +4,13 @@ import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
 import '@fortawesome/fontawesome-free/scss/regular.scss';
 import '@fortawesome/fontawesome-free/js/all';
 import './core.scss'
-<<<<<<< HEAD
-import Page from './layouts/main';
-import Header from './components/Header';
-import Footer from './components/Footer';
-=======
 import Login from './pages/login';
 import Dashboard from './pages/dashboard';
 import {Switch, Route} from 'react-router-dom';
 import { withRouter } from 'react-router-dom'
->>>>>>> ee9f5b7c1f6e049b8c3edac2781fce281d69fc54
 
 
 class App extends React.Component {
-<<<<<<< HEAD
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     isAuthenticated:false
-  //   };
-  // }
-
-  // userHasAuthenticated = authenticated => {
-  //   this.setState({isAuthenticated: authenticated});
-  // }
-  // 
-  //   const childProps = {
-  //     isAuthenticated: this.state.isAuthenticated,
-  //     userHasAuthenticated: this.userHasAuthenticated
-  //   };
-=======
   constructor(props) {
     super(props)
     this.state = {
@@ -47,7 +24,6 @@ class App extends React.Component {
       this.props.history.push("/login");
     }
   }
->>>>>>> ee9f5b7c1f6e049b8c3edac2781fce281d69fc54
   render() {
     return (
       <Switch>
@@ -60,8 +36,4 @@ class App extends React.Component {
   }
 }
 
-<<<<<<< HEAD
-export default App;
-=======
 export default withRouter(App);
->>>>>>> ee9f5b7c1f6e049b8c3edac2781fce281d69fc54
